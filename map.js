64b9b3b6b13dda389bf526ebd4e9484ea90c8043
Mohit Gupta <itsmohitt@gmail.com>
2018-05-10 22:55:58 +0530

    Working with OpenLayers 4 | Part 2 - Using markers or points on the map

--- map.js
+++ map.js
@@ -1,12 +1,27 @@
- var map = new ol.Map({
-        target: 'map',
-        layers: [
-          new ol.layer.Tile({
-            source: new ol.source.OSM()
-          })
-        ],
-        view: new ol.View({
-          center: ol.proj.fromLonLat([-74.006,40.712]), // Coordinates of Paris
+ 
+var baseMapLayer = new ol.layer.Tile({
+  source: new ol.source.OSM()
+});
+var map = new ol.Map({
+  target: 'map',
+  layers: [ baseMapLayer],
+  view: new ol.View({
+          center: ol.proj.fromLonLat([-74.0061,40.712]), 
           zoom: 7 //Initial Zoom Level
         })
-      });
+});
+
+var marker = new ol.Feature({
+  geometry: new ol.geom.Point(
+    ol.proj.fromLonLat([-74.006,40.7127])
+  ),  // Cordinates of New York's Town Hall
+});
+
+
+var vectorSource = new ol.source.Vector({
+  features: [marker]
+});
+var markerVectorLayer = new ol.layer.Vector({
+  source: vectorSource,
+});
+map.addLayer(markerVectorLayer);
