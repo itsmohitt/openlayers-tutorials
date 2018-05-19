cf80e6a21949fb406332b2c19d9069b804805cd6
Mohit Gupta <itsmohitt@gmail.com>
2018-05-19 23:52:37 +0530

    Working with OpenLayers 4 | Part 3- Setting customised markers and images on map

--- map.js
+++ map.js
@@ -6,17 +6,24 @@ var map = new ol.Map({
   target: 'map',
   layers: [ baseMapLayer],
   view: new ol.View({
-          center: ol.proj.fromLonLat([-74.0061,40.712]), 
-          zoom: 7 //Initial Zoom Level
+    center: ol.proj.fromLonLat([-74.0061,40.712]), 
+          zoom: 12 //Initial Zoom Level
         })
 });
 
 var marker = new ol.Feature({
   geometry: new ol.geom.Point(
     ol.proj.fromLonLat([-74.006,40.7127])
-  ),  // Cordinates of New York's Town Hall
+  ),  // Cordinates of New York's City Hall
 });
 
+marker.setStyle(new ol.style.Style({
+  image: new ol.style.Icon( ({
+    color: '#ffcd46',
+    crossOrigin: 'anonymous',
+    src: 'dot.png'
+  }))
+}));
 
 var vectorSource = new ol.source.Vector({
   features: [marker]
@@ -25,3 +32,16 @@ var markerVectorLayer = new ol.layer.Vector({
   source: vectorSource,
 });
 map.addLayer(markerVectorLayer);
+
+var north = 40.7127;
+var south = 40.7129;
+var east = -74.0059;
+var west = -74.0061;
+var extent = ol.proj.transformExtent([east, north, west, south], 'EPSG:4326', 'EPSG:3857');
+var imageLayer = new ol.layer.Image({
+  source: new ol.source.ImageStatic({
+                url: 'photo.png',
+                imageExtent: extent
+              })
+});
+map.addLayer(imageLayer);
